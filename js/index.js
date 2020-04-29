646c6f0edef82a76d186e3fcfb55e7c9506446cb
Lani Jung <lani009@naver.com>
2020-04-29 09:37:53 +0900

    solid

--- js/index.js
+++ js/index.js
@@ -12,9 +12,11 @@ $.each(td, function(index, item) {
         $(link+index).effect("shake", {times: 3, distance: 5}, 500);
     });
     $(item).mouseenter(function() {
-        $(item).css("background-color", "#ffb1b1").show("slow");
+        $(item).stop().animate({backgroundColor: "#ffb1b1"}, 100);
+        // $(item).css("background-color", "#ffb1b1").show("slow");
     });
     $(item).mouseleave(function() {
-        $(item).css("background-color", "#FFF").show("slow");
+        $(item).stop().animate({backgroundColor: "#FFF"}, 100);
+        // $(item).css("background-color", "#FFF").show("slow");
     });
 });
